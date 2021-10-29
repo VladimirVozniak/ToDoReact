f8a544564d3d78067974a5a8c4fa9941a007b9e1
VladimirVozniak <87637075+VladimirVozniak@users.noreply.github.com>
2021-10-29 11:24:46 +0300

    Delete src directory

--- src/Components/AddTodo/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css"
-
-const AddTodo = (props) => {
-    return (
-        <button className="add-task" onClick={() => props.addTodo()}>Add Task</button>
-    )
-}
-
-export default AddTodo;
