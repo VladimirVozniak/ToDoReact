c3fa631b26ede11087d41f4c9e0461800f95e3c8
VladimirVozniak <87637075+VladimirVozniak@users.noreply.github.com>
2021-10-29 14:47:16 +0300

    Add files via upload

---
+++ src/Components/ToDo/index.js
@@ -0,0 +1,35 @@
+import "./style.css"
+import {useEffect, useReducer, useState} from "react";
+import ToDoList from "../ToDoList";
+import InputData from "../Input";
+import AddTodo from "../AddTodo";
+import {Context} from "../../context";
+import {initialTodo, reducer} from "../../reducer";
+
+const Todo = () => {
+    const [state, dispatch] = useReducer(reducer, initialTodo)
+    const [value, setValue] = useState('')
+
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(state))
+    })
+
+    return (
+        <Context.Provider value={{state, dispatch, value, setValue}}>
+            <div className='container'>
+                <p className='text'>ToDo</p>
+                <div className="adding-todo">
+                    <InputData/>
+                    <AddTodo/>
+                </div>
+                {state.length !== 0 &&
+                <div className="todo-group">
+                    <ToDoList/>
+                </div>
+                }
+            </div>
+        </Context.Provider>
+    )
+}
+
+export default Todo;
---
+++ src/Components/ToDoList/index.js
@@ -0,0 +1,34 @@
+import "./style.css"
+import {Context} from "../../context";
+import {useContext} from "react";
+
+const ToDoList = () => {
+    const {dispatch, state} = useContext(Context)
+    return (
+        <>
+            {state.map((elem, index) =>
+                <div key={index} className="todo-element">
+                    <input type="checkbox"
+                           checked={elem.isChecked}
+                           onChange={() => dispatch({type: "checked", id: index})}
+                           className="checkbox"/>
+                    <input className={elem.isChecked ? 'todo-text todo-text_disable' : 'todo-text'}
+                           value={elem.value}
+                           onChange={(e) => dispatch({type: "edit", newText: e.target.value, id: index})}
+                           disabled={elem.isChecked}/>
+                    <button className="delete-icon"
+                            onClick={() => dispatch({type: "remove", id: index})}>
+                        <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="trash-alt"
+                             className="svg-inline--fa fa-trash-alt fa-w-14 " role="img"
+                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <path fill="currentColor"
+                                  d="M268 416h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12zM432 80h-82.41l-34-56.7A48 48 0 0 0 274.41 0H173.59a48 48 0 0 0-41.16 23.3L98.41 80H16A16 16 0 0 0 0 96v16a16 16 0 0 0 16 16h16v336a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128h16a16 16 0 0 0 16-16V96a16 16 0 0 0-16-16zM171.84 50.91A6 6 0 0 1 177 48h94a6 6 0 0 1 5.15 2.91L293.61 80H154.39zM368 464H80V128h288zm-212-48h24a12 12 0 0 0 12-12V188a12 12 0 0 0-12-12h-24a12 12 0 0 0-12 12v216a12 12 0 0 0 12 12z"/>
+                        </svg>
+                    </button>
+                </div>
+            )}
+        </>
+    )
+}
+
+export default ToDoList;
