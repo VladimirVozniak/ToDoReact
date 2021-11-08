de2484a2988344c0962de235d8dba26598143858
VladimirVozniak <87637075+VladimirVozniak@users.noreply.github.com>
2021-11-08 17:17:09 +0300

    Update todoController.js

--- Backend/controllers/todoController.js
+++ Backend/controllers/todoController.js
@@ -1,26 +1,26 @@
 const Task = require('../models/Todo')
 
-module.exports.getTodos = (req, res) => {
-    Task.find().then(result => {
+module.exports.getTodos = async (req, res) => {
+    await Task.find().then(result => {
         res.send({data: result})
     }).catch(err => res.send(err))
 };
 
-module.exports.createNewTodo = (req, res) => {
+module.exports.createNewTodo = async (req, res) => {
     const task = new Task(req.body)
-    task.save().then(result => {
+    await task.save().then(result => {
         res.send({data: result})
     }).catch(err => res.send(err))
 };
 
-module.exports.changeTodoInfo = (req, res) => {
-    Task.findOneAndUpdate({_id: req.query.id}, req.body).then(() => {
+module.exports.changeTodoInfo = async (req, res) => {
+   await Task.findOneAndUpdate({_id: req.query.id}, req.body).then(() => {
         res.send({data: "id: " + req.query.id + " deleted"})
     }).catch(err => res.send(err))
 };
 
-module.exports.deleteTodo = (req, res) => {
-    Task.deleteOne({_id: req.query.id}).then(() => {
+module.exports.deleteTodo = async (req, res) => {
+    await Task.deleteOne({_id: req.query.id}).then(() => {
         res.send({data: "id: " + req.query.id + " deleted"})
     }).catch(err => res.send(err))
 };
