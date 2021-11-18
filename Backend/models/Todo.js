0e6a8a69ee10eb3c02ca706c5a2dc712d3eb6cc0
VladimirVozniak <87637075+VladimirVozniak@users.noreply.github.com>
2021-11-18 15:14:17 +0300

    Delete Backend directory

--- Backend/models/Todo.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require('mongoose')
-
-const todoScheme = new Schema({
-    value: String,
-    isChecked: Boolean,
-    user: String
-}, {versionKey: false})
-
-module.exports = model("todos", todoScheme)
