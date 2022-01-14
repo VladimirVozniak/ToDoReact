1c15b411a9c490abf9c0d3cee816f4783cb783c6
VladimirVozniak <87637075+VladimirVozniak@users.noreply.github.com>
2022-01-14 10:20:09 +0300

    Delete Backend directory

--- Backend/models/User.js
+++
@@ -1,8 +0,0 @@
-const {Schema,model} = require("mongoose")
-
-const userScheme = new Schema({
-    login: String,
-    password: String,
-},{versionKey: false})
-
-module.exports = model("users", userScheme)
